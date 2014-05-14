758038389d9953fa45dbab57094b736ec3f91ee1
Bertrand Marron <bertrand.marron@ionis-group.com>
2014-05-14 17:47:39 +0200

    Update version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def package_data(pkg, root):
 
 setup(
     name='taggedtext-xblock',
-    version='0.0.1',
+    version='0.1.0',
     description='Tagged Text XBlock',
     author='IONISx',
     packages=['taggedtext'],
